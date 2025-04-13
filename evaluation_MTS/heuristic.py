e69d5c7ac8d0ffc6aef20a8d678d7cd3742861f6
Xinjie Zhang <xz3236@columbia.com>
2025-04-13 17:50:24 -0400

    update

--- evaluation_MTS/heuristic.py
+++ evaluation_MTS/heuristic.py
@@ -1,6 +1,13 @@
-# example heuristic
-# replace it with your own heuristic designed by EoH
+import numpy as np
 
-def score(item, bins):
-    scores = item - bins
-    return scores
+
+def place_facilities(peaks, weights, k):
+    candidate_positions = np.linspace(0, 1, 101)
+    facilities_positions = []
+    for position in candidate_positions:
+        cost = np.sum(weights * np.abs(peaks - position))
+        facilities_positions.append((position, cost))
+    facilities_positions.sort(key=lambda x: x[1])
+    best_positions = [fac_position[0] for fac_position in facilities_positions[:k]]
+
+    return facilities_positions
